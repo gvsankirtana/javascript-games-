827f0d1d2f1b18684b41fc7a94fc2166dd3aad22
Sankirtana <59257797+geek-2002@users.noreply.github.com>
2020-06-23 20:59:35 +0530

    Add files via upload

--- drum kit/index.js
+++ drum kit/index.js
@@ -1,5 +1,45 @@
-for(i=0;i<=document.querySelectorAll(".drum").length;i++){
-document.querySelectorAll(".drum")[i].addEventListener("click",function(){
-    alert("clicked");
-})
+
+document.addEventListener("keypress", function(event){
+ makeSound(event.key);
+});
+
+for(i=0;i<=document.querySelectorAll("button").length;i++){
+document.querySelectorAll("button")[i].addEventListener("click",function(){
+    //this.style.color="white";//this triggers all the button objects here or triggers the event
+    var buttonInnerHTML = this.innerHTML;
+    makeSound(buttonInnerHTML);
+});
+}
+function makeSound(key){
+    switch(key){
+        case "w":
+          var audio = new Audio('sounds/tom-1.mp3');
+          audio.play();
+          break;
+        case "a":
+          var audio = new Audio('sounds/tom-2.mp3');
+          audio.play();
+          break;
+        case "s":
+          var audio = new Audio('sounds/tom-3.mp3');
+          audio.play();
+          break; 
+        case "d":
+          var audio = new Audio('sounds/tom-4.mp3');
+          audio.play();
+          break;
+        case "j":
+          var audio = new Audio('sounds/crash.mp3');
+          audio.play();
+          break;
+        case "k":
+          var audio = new Audio('sounds/kick-bass.mp3');
+          audio.play();
+          break;
+        case "l":
+          var audio = new Audio('sounds/snare.mp3');
+          audio.play();
+          break;
+          default: console.log(buttonInnerHTML);
+      }
 }
